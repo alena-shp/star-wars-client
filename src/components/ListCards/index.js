58acd706cd06a951a1a1889402152468a3100220
Shpakova Alena <shpakovadev@gmail.com>
2021-02-16 19:47:48 +0300

    add CardItem, ListCard

---
+++ src/components/ListCards/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CardItem from 'components/CardItem'
+
+const ListCards = () => {
+  return (
+    <div>
+      <CardItem />
+    </div>
+  )
+}
+
+export default ListCards
